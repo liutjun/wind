3186c8f055bd4ec77dc5c5819f518b9f50a5589c
wind <3313538471@qq.com>
2024-07-01 09:39:33 +0800

    第一个提交方法

---
+++ src/test/java/com/liu/git_demo001/GitDemo001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liu.git_demo001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDemo001ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
