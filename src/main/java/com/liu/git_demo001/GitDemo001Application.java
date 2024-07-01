c0656090b4977c71e1d869a1e6ea0c05521f3ff1
wind <3313538471@qq.com>
2024-07-01 09:46:15 +0800

    第一个提交方法

--- src/main/java/com/liu/git_demo001/GitDemo001Application.java
+++ src/main/java/com/liu/git_demo001/GitDemo001Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitDemo001Application {
 
     public static void main(String[] args) {
+        System.out.println("hello world");
         SpringApplication.run(GitDemo001Application.class, args);
     }
 
